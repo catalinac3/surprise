cf05c1f28febf270ec0c90762661020e7039b1bd
catalina <cdianacatalina@gmail.com>
2020-10-23 09:52:44 +0200

    added changes from pullrequest

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 const rootUrl = "https://api.edamam.com/search"
 const recipeUrlElement = document.querySelector("#recipe-url")
 
-let urlApi = `${rootUrl}?q=birthday+cake&app_id=589ecbd6&app_key=6d6116bfcbdc60fe641222727dc9eb8f&from=0&to=1`
+const apiUrl = `${rootUrl}?q=birthday+cake&app_id=589ecbd6&app_key=6d6116bfcbdc60fe641222727dc9eb8f&from=0&to=1`
 
-fetch(urlApi)
+fetch(apiUrl)
 .then((response) => response.json())
 .then((data) => {
   console.log('Success:', data);
